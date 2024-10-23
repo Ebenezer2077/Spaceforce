a6fd6376a9f34836448120b600ccf5b3a4aafb1f
Ebenezer2077 <tobigcb547@outlook.de>
2024-10-23 20:51:58 +0200

    first steps

--- Functions/ship_functions.mjs
+++ Functions/ship_functions.mjs
@@ -1,11 +1,19 @@
 export function createShip(ctx, x, y, radius, callback) {
-
+    let isTouched, identifier
 
     function draw() {
         circle(ctx, x, y, radius, "blue");
     }
 
-    return { draw };
+    function Touched(id, tx, ty) {
+        touched = distance(x, y, tx, ty) < radius;
+        if(touched) {
+            identifier = id;
+            callback();
+        }
+    }
+
+    return { draw, Touched };
 }
 
 function circle(ctx, x, y, radius, color) {
