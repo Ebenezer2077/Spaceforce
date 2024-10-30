7542d1410f322e1c6d5f8d502b2a566b9276cd3b
Ebenezer2077 <tobigcb547@outlook.de>
2024-10-30 20:34:36 +0100

    testing on touch device

--- Functions/ship_functions.mjs
+++ Functions/ship_functions.mjs
@@ -1,18 +1,30 @@
-export function createShip(canvas ,ctx, x, y, radius, callback) {
+export function createShip(canvas ,ctx, x1, y1, radius, callback) {
 
-    let touched, identifier
-    let position = [x, y];
+    let touched, identifierONE = undefined, identifierTWO = undefined;
+    let position = [x1, y1, 0, 0];
+    let PATH = new Path2D();
+    let angle = 0;
 
     function draw() {
         draw_spaceship(position[0], position[1]);
-        //circle(ctx, position[0], position[1], radius, "blue");
     }
 
     function Touched(id, tx, ty) {
-        touched = distance(position[0], position[1], tx, ty) < radius;
-        if(touched) {
-            identifier = id;
-            callback();
+        if(identifierONE === undefined) {
+            touched = ctx.isPointInPath(PATH, tx, ty);
+            if(touched) {
+                identifierONE = id;
+                position[0] = tx;
+                position[1] = ty;
+            }
+        } else {
+            if(identifierTWO === undefined) {
+                identifierTWO = id;
+                position[2] = tx;
+                position[3] = ty;
+                angle = Math.atan2(position[3] - position[1], position[2] - position[0]);
+                console.log(angle);
+            }
         }
     }
 
@@ -21,31 +33,34 @@ export function createShip(canvas ,ctx, x, y, radius, callback) {
         console.log("momentarily Position: ", position);
     }
 
-    function draw_spaceship(x,y) {
+    function draw_spaceship(x,y, angle = 0) {
+        ctx.translate(x,y);
+        ctx.rotate(angle);
         var sWidth = canvas.width;
         var sHeight = canvas.height;
         var path = new Path2D();
         //triangle
-        path.moveTo(x+12, y);                       //right
-        path.lineTo(x, y-30);                       //top
-        path.lineTo(x-12, y);                       //left
+        path.moveTo(12, 0);                       //right
+        path.lineTo(0, -30);                       //top
+        path.lineTo(-12, 0);                       //left
 
         //rectangle
-        path.lineTo(x-12, y+40);                    //left-bottom           
-        path.lineTo(x+12, y+40);                    //right-bottom
+        path.lineTo(-12, 40);                    //left-bottom           
+        path.lineTo(12, 40);                    //right-bottom
 
         //Wing
-        path.lineTo(x+40, y+40);                    //right-wing
-        path.lineTo(x+40, y+55); 
-        path.lineTo(x-40, y+55);                     //left-wing
-        path.lineTo(x-40, y+40);
-        path.lineTo(x+12, y+40);  
+        path.lineTo(40, 40);                    //right-wing
+        path.lineTo(40, 55); 
+        path.lineTo(-40, 55);                     //left-wing
+        path.lineTo(-40, 40);
+        path.lineTo(12, 40);  
 
 
 
 
 
         ctx.fill(path);                 //End figure
+        PATH = path;
     }
 
 
